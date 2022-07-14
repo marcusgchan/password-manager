1262a3c8a63788c44047d1e63d6dd7eeae0e2bef
Marcus <marcuscchan@gmail.com>
2022-07-13 20:39:44 -0700

    updated baseurl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { NextComponentType } from "next";
 import { withTRPC } from "@trpc/next";
 import { AppRouter } from "../server/routers/app";
 import { SnackbarProvider } from "../contexts/SnackbarContext";
+import superjson from "superjson";
 
 type CustomPageProps = AppProps & {
   Component: NextComponentType & { auth?: boolean };
@@ -50,18 +51,27 @@ const Auth = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>;
 };
 
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+  if (process.browser) return ""; // Browser should use current path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
 export default withTRPC<AppRouter>({
   config({ ctx }) {
     /**
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : "http://localhost:3000/api/trpc";
+    const url = `${getBaseUrl()}/api/trpc`;
 
     return {
       url,
+      transformer: superjson,
       /**
        * @link https://react-query.tanstack.com/reference/QueryClient
        */
