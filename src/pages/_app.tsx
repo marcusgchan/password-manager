1f23438ff79e512915b43748bd07d071c2ece4d0
Marcus <marcuscchan@gmail.com>
2022-07-14 20:36:31 -0700

    added next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -57,6 +57,7 @@ const getBaseUrl = () => {
   }
   if (process.browser) return ""; // Browser should use current path
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.NEXTAUTH_URL) return `https://${process.env.NEXTAUTH_URL}`;
 
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
