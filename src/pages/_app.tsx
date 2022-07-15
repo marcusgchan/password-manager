fb3bcbbbd6bf6ab60e25a8c0e82a296a7d16e25a
Marcus <marcuscchan@gmail.com>
2022-07-14 19:37:01 -0700

    added superjson

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -67,7 +67,8 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = `${getBaseUrl()}/api/trpc`;
+    // const url = `${getBaseUrl()}/api/trpc`;
+    const url = `http://${getBaseUrl()}/api/trpc`;
 
     return {
       url,
--- src/server/routers/app.ts
+++ src/server/routers/app.ts
@@ -1,7 +1,10 @@
 import { createRouter } from "../createRouter";
 import { siteRouter } from "./site";
+import superjson from "superjson";
 
-export const appRouter = createRouter().merge("site.", siteRouter);
+export const appRouter = createRouter()
+  .transformer(superjson)
+  .merge("site.", siteRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
